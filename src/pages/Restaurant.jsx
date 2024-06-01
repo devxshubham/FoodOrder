0aebef28da3aca7789839f741afdff7f7053c244
devxshubham <devshubhamcode@gmail.com>
2024-06-01 23:15:55 +0530

    added food category in restaurant

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -5,19 +5,35 @@ import { useState } from "react";
 
 const Restaurant = () => {
     const {restaurantId} = useParams()
-    console.log(restaurantId)
+    // console.log(restaurantId)
 
-    const [menu, setMenu] = useState([])
-    console.log(menu);
+    const [restData, setRestData] = useState([])
 
     useEffect( () => {
         axios.get("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.3610314&lng=76.8485468&restaurantId="+restaurantId)
         .then( (response) => {
-            setMenu(response.data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.            )
+            // setMenu(response.data?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards            )
+            setRestData(response.data?.data.cards[4].groupedCard?.cardGroupMap?.REGULAR?.cards)
         })
     },[])
+
+    const category = restData.filter ( data => data?.card?.card?.["@type"] === 'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory' )
+    console.log(category)
+    // return <div>hi</div>
     
-    return <div>restaurant menu</div>
+    return <div className="mx-auto text-center">
+            {category.map( men => {
+                // console.log(men.card.card?.itemCards)
+                return <div > 
+                    <div className="font-bold">{men.card?.card?.title}</div>
+                    <div>
+                        {men.card.card?.itemCards?.map( item => {
+                            return <h1>{item.card.info.name}</h1>
+                        })}
+                    </div>
+                </div>
+            })}
+    </div>
     
 
 }
