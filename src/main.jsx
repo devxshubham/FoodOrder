5a5f11865a7ad70874bced21e2eba28f905b5c9f
devxshubham <devshubhamcode@gmail.com>
2024-06-03 21:27:44 +0530

    setting up redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,34 +2,46 @@ import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import { Provider } from 'react-redux'
+
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './pages/Home.jsx'
 import { About } from './pages/About.jsx'
 import Restaurant from './pages/Restaurant.jsx'
-import App from './App.jsx'
+import appStore from './lib/appStore'
+import Home from './pages/Home'
+import App from './App'
 const Grocery = lazy( ()=> import('./pages/Grocery.jsx'))
+
 
 const myRouter = createBrowserRouter([
   {
     path : '/',
-    element : <App/>
-  },
-  {
-    path : '/restaurant/:restaurantId',
-    element : <Restaurant/>
-  },
-  {
-    path : '/about',
-    element : <About/>
-  },
-  {
-    path : '/grocery',
-    element : <Suspense fallback={<h1>....loading</h1>}> <Grocery/> </Suspense>
+    element : <App/>,
+    children : [
+      {
+        path : '/',
+        element : <Home/>
+      },
+      {
+        path : '/restaurant/:restaurantId',
+        element : <Restaurant/>
+      },
+      {
+        path : '/about',
+        element : <About/>
+      },
+      {
+        path : '/grocery',
+        element : <Suspense fallback={<h1>....loading</h1>}> <Grocery/> </Suspense>
+      }
+    ]
   }
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={myRouter}></RouterProvider>
 
+  <Provider store={appStore}>
+    <RouterProvider router={myRouter}></RouterProvider>
+  </Provider>
   
 )
